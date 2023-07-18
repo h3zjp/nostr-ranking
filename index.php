2406aba2147ae9dbc485d0468777b32c59912063
七海灯 <uso800@h3z.jp>
2023-07-19 00:39:11 +0900

    DAU表示を追加
    Daily、kind 1 の場合のみ DAU を表示するよう処理を追加

--- index.php
+++ index.php
@@ -163,24 +163,34 @@
 
 		}
 
-			$mfile = fopen($file1, 'r');
-			$mcsv = fgetcsv($mfile, 1);
-			$mget_time = $mcsv[0];
-			while ($line = fgetcsv($mfile)) {
-				$data[] = array_combine(Array('0','1'), $line);
-			}
-			$count = count($data);
-			fclose($mfile);
+		$mfile = fopen($file1, 'r');
+		$mcsv = fgetcsv($mfile, 1);
+		$mget_time = $mcsv[0];
+		while ($line = fgetcsv($mfile)) {
+			$data[] = array_combine(Array('0','1'), $line);
+		}
+		$count = count($data);
+		fclose($mfile);
 
-			$mfile2 = fopen($file2, 'r');
-			$mcsv2 = fgetcsv($mfile2, 1);
-			$mget_time2 = $mcsv2[0];
-			while ($line2 = fgetcsv($mfile2)) {
-				$username[] = array_combine(Array('0','1','2'), $line2);
-			}
-			fclose($mfile2);
+		$mfile2 = fopen($file2, 'r');
+		$mcsv2 = fgetcsv($mfile2, 1);
+		$mget_time2 = $mcsv2[0];
+		while ($line2 = fgetcsv($mfile2)) {
+			$username[] = array_combine(Array('0','1','2'), $line2);
+		}
+		fclose($mfile2);
 
-		echo '<h2>' . $title1 . $title2 . "ランキング</h2>\n";
+		if ($_GET['ranking'] == 'daily' && $_GET['kind'] == '1') {
+
+			$title3 = "(DAU：" . $count . ")";
+			echo '<h2>' . $title1 . $title2 . "ランキング " . $title3 . "</h2>\n";
+
+		} else {
+
+			echo '<h2>' . $title1 . $title2 . "ランキング</h2>\n";
+
+		}
+
 		echo "<table>\n";
 		echo "	<tr>\n";
 		echo "		<td></td>\n";
